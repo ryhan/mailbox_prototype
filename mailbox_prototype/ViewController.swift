fd965db2743f389202336dd8b69a136dac23d4ee
Ryhan Hassan <ryhanh@me.com>
2014-08-24 17:26:31 -0700

    Initial Commit

---
+++ mailbox_prototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mailbox_prototype
+//
+//  Created by Ryhan Hassan on 8/24/14.
+//  Copyright (c) 2014 Ryhan Hassan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
